daa4c296541245326d4b2cc95dae2b5b087b9557
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-08 05:00:20 -0400

    likely needed

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/foot.svg
 export DESKTOP=/usr/share/applications/foot.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun \
