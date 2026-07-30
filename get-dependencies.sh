5e8f54b30313debc71c69a574635754b366e5ff8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-07-30 14:15:30 -0400

    there is a better fork of `12to11` now

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,17 +28,4 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# also build 12to11 for x11 compat
-git clone --depth 1 https://git.linuxping.win/12to11/12to11 ./12to11
-cd ./12to11
-# fix transparency issue with EGL renderer
-# this will break compat with nvidia prop driver unfortunately
-sed -i \
-	-e 's|egl_config_attribs\[1\] = 24|egl_config_attribs\[1\] = 32|' \
-	-e 's|egl_config_attribs\[9\] = 0|egl_config_attribs\[9\] = 8|'   \
-	./egl.c
-xmkmf
-make -j"$(nproc)"
-install -Dm755 12to11 /usr/bin/12to11
+make-aur-package 12to11-git
